e736d3d4b7d0973b8f95f5746c9eef0b4aad575e
joy <chengfg@bc.edu>
2020-03-12 00:37:11 -0400

    Initial Commit

---
+++ Weather Gift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather Gift
+//
+//  Created by cheng jiayi on 2020/3/12.
+//  Copyright © 2020 cheng jiayi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
