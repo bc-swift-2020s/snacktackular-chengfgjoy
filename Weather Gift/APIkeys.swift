5f180479082f5613090e90e415183b95e53d7f9e
joy <chengfg@bc.edu>
2020-03-15 15:36:45 -0400

    with gitignore

---
+++ Weather Gift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  Weather Gift
+//
+//  Created by cheng jiayi on 2020/3/15.
+//  Copyright © 2020 cheng jiayi. All rights reserved.
+//
+
+import Foundation
+
+struct APIkets{
+    static let googlePlacesKey = "AIzaSyDDpAvTBYjJO_r73UFKizIMlJGkpbp8Km0"
+}
---
+++ Weather Gift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  Weather Gift
+//
+//  Created by cheng jiayi on 2020/3/12.
+//  Copyright © 2020 cheng jiayi. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation{
+    var name : String
+    var latitude : Double
+    var longtitude: Double
+   }
